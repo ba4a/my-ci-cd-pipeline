dcc337028ff6c3f903cb67a3f053465c71a57bb6
Mohamed Waleed <139256760+mohamedwaleed77@users.noreply.github.com>
2024-10-07 15:58:51 +0300

    Update isON.py

--- test_scripts/isON.py
+++ test_scripts/isON.py
@@ -1,11 +1,5 @@
 import requests
 
-def check_website_status(url):
-        response = requests.get(url)
-        if response.status_code == 200:
-            print(f'The website {url} is online.')
-        else:
-            print(f'The website {url} returned status code: {response.status_code}')
-
-website_url = 'http://localhost:5050/petclinic'  
-check_website_status(website_url)
+website_url = 'http://localhost:5050/petclinic'
+response = requests.get(website_url)
+print(f'The website {website_url} is online with status code: {response.status_code}')
